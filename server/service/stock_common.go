cf4e65ac99f3660009fe509b9a6de475be69f550
madhatter <841727184@qq.com>
2020-06-06 19:15:01 +0800

    配置

--- server/service/stock_common.go
+++ server/service/stock_common.go
@@ -9,39 +9,34 @@ type stockCommonService struct {
 }
 
 func (srv *stockCommonService) CheckOffday() (offday bool) {
-	codes := []string{"000001"}
+	codes := []string{"000001", "601398", "600519", "601939"}
 	stockList := crawler.NewDongFangCrawler().Monitor(codes)
-
 	if len(stockList) < 1 {
 		return
 	}
 
-	stock := stockList[0]
+	for _, stock := range stockList {
+		stockPOList, err := dao.StockDao.FindByCode(stock.Code)
+		if err != nil {
+			return
+		}
+		if len(stockPOList) == 0 {
+			return
+		}
 
-	stockPOList, err := dao.StockDao.FindByCode(stock.Code)
-	if err != nil {
-		return
-	}
-	if len(stockPOList) == 0 {
-		return
-	}
+		stockPO := stockPOList[0]
 
-	stockPO := stockPOList[0]
+		if stock.Percent != stockPO.Percent {
+			return
+		}
 
-	if stock.Code != stockPO.Code {
-		return
-	}
+		if stock.Amount != stockPO.Amount {
+			return
+		}
 
-	if stock.Percent != stockPO.Percent {
-		return
-	}
-
-	if stock.Amount != stockPO.Amount {
-		return
-	}
-
-	if stock.Volume != stockPO.Volume {
-		return
+		if stock.Volume != stockPO.Volume {
+			return
+		}
 	}
 
 	offday = true
